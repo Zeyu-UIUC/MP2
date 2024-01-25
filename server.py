19164d8fb49739cd58ac36122e30d3d90a174148
Zeyu Liao <zeyu9@illinois.edu>
2024-01-25 13:41:06 +0800

    upload

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import os
+import socket
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_ip():
+    ip = socket.gethostbyname(socket.gethostname())
+    return ip
+
+@app.route('/', methods=['POST'])
+def start_SC():
+    subprocess.Popen(['python3', 'stress_cpu.py'])
+    return jsonify({'message': 'Process stress CPU.'})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
